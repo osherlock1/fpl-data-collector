2f0e82801c4b853727f7f96bf479ef6f09c04318
osherlock1 <omalley.j.sherlock@gmail.com>
2025-09-01 13:51:28 -0400

    add scotter plot method

--- main.py
+++ main.py
@@ -2,12 +2,17 @@
 
 from fpl_data_manager import FPLDataManager
 from cli_config import cli_args
+from plotter import PlotManager
+
 
 def main():
 
     args = cli_args() #get cli args
     databse_url = 'FPL_data.db'
     db_manager = FPLDataManager(databse_url)
+
+    #plot manager instance
+    plt_mng = PlotManager()
 
     #Update Database
     if args.update_db:
@@ -21,15 +26,30 @@ def main():
     #Run analysis and plot
     if args.analyze:
 
-        top_players = db_manager.get_top_players(2, 30, "total_points", order_by="DESC")
+        top_players = db_manager.get_top_players(4, 100, "total_points", order_by="DESC")
 
 
-        desired_keys = ["web_name", "total_points"]
+        desired_keys = ["web_name", "total_points", "now_cost"]
 
         for player in top_players:
             for key in desired_keys:
                 print(f"{key}: {player[key]}")
             print("---")
+
+
+        price = []
+        points = []
+        labels = []
+        for player in top_players:
+            price.append(int(player["now_cost"]) / 10)
+            points.append(player["total_points"])
+            labels.append(player["web_name"])
+        
+        plt_mng.plot_scatter(x = price , y = points, labels = labels)
+
+                
+                
+
 
 if __name__ == "__main__":
     main()
--- plotter.py
+++ plotter.py
@@ -2,9 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-#TODO: WORK ON THE PLOTTER
-
 class PlotManager:
     def __init__(self):
-
         pass
+
+    def plot_scatter(self, x , y, labels, title ="Scatter Plot", xlabel = "X", ylabel="Y"):
+        plt.figure(figsize=(10,6))
+        plt.scatter(x, y)
+        for i, label in enumerate(labels):
+            plt.annotate(label, (x[i], y[i]), fontsize = 8, ha='right')
+        plt.title(title)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.show()
